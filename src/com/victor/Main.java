3d966dd495b7284ff2ef1096428cc1a14e87428e
Victor Suciu <victorsuciu20@gmail.com>
2019-02-03 14:49:39 -0800

    changes

--- src/com/victor/Main.java
+++ src/com/victor/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         //Test
+        System.out.println(1 + 2);
         for(int i = 0; i < 10; i++) {
             System.out.println(i);
         }
