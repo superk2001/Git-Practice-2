66c8265d4202647cc27b3744f4d87a44e6baa71e
Victor Suciu <victorsuciu20@gmail.com>
2019-02-03 14:34:44 -0800

    Revolutionary new feature

--- src/com/victor/Main.java
+++ src/com/victor/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         //Test
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
 
